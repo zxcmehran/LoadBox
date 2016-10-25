095a968e2bcb7fb2dfd914f70cd1c39359253f8e
zxcmehran <zxcmehran@gmail.com>
2016-10-25 21:14:53 +0330

    Added a workaround to support ext4 USB flash hotplugging.

--- sh/flash-hotplug.sh
+++ sh/flash-hotplug.sh
@@ -17,11 +17,22 @@ if [ "$DOWNLOAD_HOTPLUG" != "" ]; then
 	echo "***[hotplug]*** Plugged in." | grep --color '.'
 	cd "$(dirname "$0")"/..
 	touch usbreadsucc
-	
+
+	echo "***[hotplug]*** Trying to mount as FAT32/NTFS" | grep --color '.'
 	mount "$1" "$DOWNLOAD_HOTPLUG" -o uid=$(id -u $ARIA2C_WORKING_USER),gid=$(id -g $ARIA2C_WORKING_USER),utf8,dmask=027,fmask=137
+	
+	if  [ "$?" -ne 0 ]
+	then 
+		echo "***[hotplug]*** No Luck. Trying to mount as EXT3/4" | grep --color '.'
+		# If it's ext4, uid and gid would be obsolete.
+		mount "$1" "$DOWNLOAD_HOTPLUG"
+	fi
+
 	ls "$DOWNLOAD_HOTPLUG" >/dev/null 2>&1
 	
-	if  [ "$?" -eq 0 ] && [ `stat -fc%t:%T "$DOWNLOAD_HOTPLUG"` != `stat -fc%t:%T "$DOWNLOAD_HOTPLUG/.."` ]; then
+	if  [ "$?" -eq 0 ] && mountpoint -q -- "$DOWNLOAD_HOTPLUG"; then
+		echo "***[hotplug]*** Mounting done." | grep --color '.'
+
 		if ps ax | grep -v grep | grep aria2c > /dev/null
 		then
 			echo "***[hotplug]*** Resuming downloads." | grep --color '.'
